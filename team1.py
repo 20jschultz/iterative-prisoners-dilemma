2338cb2898cae76d133b854088a265b8ebb98495
stephanothecow <stephanothecow@gmail.com>
2016-12-13 14:18:05 -0700

    Added Strategy
    Set the strategy to always collude.

--- team1.py
+++ team1.py
@@ -7,28 +7,23 @@
 ####
 
 team_name = 'The McSwaggins fam' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
-def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+strategy_name = 'Collude'
+strategy_description = 'Always collude.'
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+def move(my_history, their_history, my_score, their_score):
+    '''Make my move based on the history with this player.
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
+    history: a string with one letter (c or b) per round that has been played with this opponent.
+    their_history: a string of the same length as history, possibly empty. 
+    The first round between these two players is my_history[0] and their_history[0]
+    The most recent round is my_history[-1] and their_history[-1]
     
+    Returns 'c' or 'b' for collude or betray.
+    '''
+    
+    # This player always colludes.
     return 'c'
 
-    
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
